7c94065e13d6f3595c61381025795c995ac7b4b6
Deekshitha <deekshithas37@gmail.com>
2023-12-04 18:36:56 +0530

    search

--- src/components/customer/components/cars.js
+++ src/components/customer/components/cars.js
@@ -8,62 +8,57 @@ import eternity from "../../../assets/car.jpg"
 // import { Card } from "react-bootstrap/esm";
 // import {  CardBody, CardText, CardTitle } from "react-bootstrap";
 // import { CardSubtitle } from "react-bootstrap";
-function Cars(){
+function Cars() {
   const [source, setSource] = useState('');
   const [cars, setCars] = useState([]);
-  const location = useLocation(); // Use useLocation hook
+  const location = useLocation();
 
   useEffect(() => {
-    // Retrieve source from query parameters
     const searchParams = new URLSearchParams(location.search);
     const sourceFromQuery = searchParams.get("source");
-    console.log("Source from query:", sourceFromQuery);
+
     if (sourceFromQuery) {
       setSource(sourceFromQuery);
-      // Now you can use the 'sourceFromQuery' in your API call
+
       axios.get(`http://localhost:9191/car/get/availablecars/${sourceFromQuery}`)
         .then(response => setCars(response.data))
         .catch(error => console.error('Error fetching available cars:', error));
     }
-  }, [location.search]); // Add location.search to the dependency array
+  }, [location.search]);
 
-    return(
-        <div>
-         
-         <div>
-    <NavbarComponent />
-    <h3 style={{ color: "Green" ,fontWeight:"bold"}}> Available Cars</h3>
-</div>
-<div className="row" style={{ paddingLeft: "56px" }}>
-              {cars.map((p, index) => (
-                <div key={index} className="col-md-4 mb-4">
-                  <Card
-                    style={{
-                      width: "25rem",
-                      height: "15rem",
-                      padding:"1px",
-                      borderColor:"darkmagenta"
-                    }}
-                  >
-                    {/* <center><Image width={280} src={eternity} /></center> */}
-                    <CardBody >
-                      <CardTitle><h3>{p.carModel}</h3></CardTitle>
-                      <CardSubtitle  style={{ color: "red" ,textAlign:"center"}}>
-                        Rent Price(per day): INR. {p.price}
-                      </CardSubtitle>
-                      <br></br>
-                      <CardText style={{ color: "blue" ,textAlign:"center", fontWeight:"500"}}>seating:{p.seating}</CardText>
-                      {/* <CardText style={{ color: "purple" ,textAlign:"center", fontWeight:"500"}}>insurance:{p.insurance}</CardText> */}
-                      <CardText style={{ color: "magenta" ,textAlign:"center" , fontWeight:"500"}}>source:{p.source}</CardText>
-                      <Button as={Link} to="/customer/bookcar" >Book Car</Button>
-                    </CardBody>
-                  </Card>
-                  <Nav.Link> </Nav.Link>
-                </div>
-              ))}
-            </div>
-
+  return (
+    <div>
+      <div>
+        <NavbarComponent />
+        <h3 style={{ color: "Green", fontWeight: "bold" }}> Available Cars</h3>
       </div>
-    )
+      <div className="row" style={{ paddingLeft: "56px" }}>
+        {cars.map((p, index) => (
+          <div key={index} className="col-md-4 mb-4">
+            <Card
+              style={{
+                width: "25rem",
+                height: "15rem",
+                padding: "1px",
+                borderColor: "darkmagenta"
+              }}
+            >
+              <CardBody>
+                <CardTitle><h3>{p.carModel}</h3></CardTitle>
+                <CardSubtitle style={{ color: "red", textAlign: "center" }}>
+                  Rent Price(per day): INR. {p.price}
+                </CardSubtitle>
+                <br></br>
+                <CardText style={{ color: "blue", textAlign: "center", fontWeight: "500" }}>seating:{p.seating}</CardText>
+                <CardText style={{ color: "magenta", textAlign: "center", fontWeight: "500" }}>source:{p.source}</CardText>
+                <Button as={Link} to="/customer/bookcar">Book Car</Button>
+              </CardBody>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+
 export default Cars;
--- src/components/customer/components/navbar.js
+++ src/components/customer/components/navbar.js
@@ -1,31 +1,55 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { useState } from "react";
+import { Container, Nav, Navbar, Form, FormControl, Button } from "react-bootstrap";
 import { useNavigate } from "react-router";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";  // Import the search icon from Font Awesome
 
 function NavbarComponent() {
   const navigate = useNavigate();
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearch = () => {
+    // Redirect to cars page with the search query
+    navigate(`/customer/cars?source=${searchValue}`);
+  };
 
   return (
     <div className="mb-4 navstyle">
-      <Navbar  >
-        <Container  >
-        <Navbar.Brand href="#home" style={{color: "red"}} onClick={() => navigate('/home')} className=" home "><h4>CarRent</h4></Navbar.Brand>
-          <Navbar.Brand href="#home" style={{color: "white"}}onClick={() => navigate('/home')} className=" home "><h4>🏠Home</h4></Navbar.Brand>
-          
-          <Nav className="me-auto">
-            <Nav.Link style={{color: "white"}} onClick={() => navigate('/customer/dashboard')}><h4>CustomerDashboard</h4></Nav.Link>
+      <Navbar>
+        <Container>
+          <Navbar.Brand href="#home" style={{ color: "red" }} onClick={() => navigate('/home')} className=" home ">
+            <h4>CarRent</h4>
+          </Navbar.Brand>
+          <Navbar.Brand href="#home" style={{ color: "white" }} onClick={() => navigate('/home')} className=" home ">
+            <h4>🏠Home</h4>
+          </Navbar.Brand>
 
-            
-            
+          <Nav className="me-auto">
+            <Nav.Link style={{ color: "white" }} onClick={() => navigate('/customer/dashboard')}>
+              <h4>CustomerDashboard</h4>
+            </Nav.Link>
           </Nav>
+
+          <Form className="d-flex">
+            <FormControl
+              type="text"
+              placeholder="Search by source"
+              className="mr-2"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+            />
+            <Button variant="outline-success" onClick={handleSearch}>
+              {/* Use the search icon here */}
+              <FaSearch />
+            </Button>
+          </Form>
+
           <Nav className="justify-content-end">
-          
-         
-          <button className="btn btn-info" onClick={() => navigate('/help')} > Help</button>
-          &nbsp;
-          <button className="btn btn-warning" onClick={() => navigate('/host/dashboard')} > Sign up as Host</button>
-          &nbsp;
-          <button className="btn btn-primary" onClick={()=>navigate('/auth/login')}>Login</button>
+            <button className="btn btn-info" onClick={() => navigate('/help')}>Help</button>
+            &nbsp;
+            <button className="btn btn-warning" onClick={() => navigate('/host/dashboard')}>Sign up as Host</button>
+            &nbsp;
+            <button className="btn btn-primary" onClick={() => navigate('/auth/login')}>Login</button>
           </Nav>
         </Container>
       </Navbar>
