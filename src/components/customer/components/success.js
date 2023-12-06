89dfa31ef0f86894fe467269d51c3b2934637c17
Deekshitha <deekshithas37@gmail.com>
2023-12-06 17:46:36 +0530

    booking success img

--- src/components/customer/components/success.js
+++ src/components/customer/components/success.js
@@ -1,12 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom"; 
 import NavbarComponent from "./navbar";
+import successImage from "../../../assets/success img.png";
 
-function Success(){
-
-    return(
+function Success() {
+    return (
         <div>
-            
+            <NavbarComponent />
             <h3>Booking successful</h3>
+            <img
+                src={successImage}
+                alt="Success"
+                style={{
+                    border: "2px ",
+                    borderRadius: "8px",
+                    marginTop: "10px",
+                    maxWidth: "10%", // Set the maximum width to maintain responsiveness
+                    height: "auto", // Allow the height to adjust proportionally
+                }}
+            />
+            
+            <div style={{ textAlign: "center", marginTop: "20px" }}>
+                <Link to="/customer/dashboard">
+                    <button style={{ padding: "10px", fontSize: "16px" }}>
+                        Back to Customer Dashboard
+                    </button>
+                </Link>
+            </div>
         </div>
-    )
+    );
 }
+
 export default Success;
