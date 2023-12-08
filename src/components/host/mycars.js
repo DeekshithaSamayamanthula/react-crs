174b8a1d2caf6e843310f66105ea7e7f7af57267
Deekshitha <deekshithas37@gmail.com>
2023-12-08 19:36:47 +0530

    customer bookings

--- src/components/host/mycars.js
+++ src/components/host/mycars.js
@@ -1,14 +1,15 @@
-// BookingHistory.js
+// MyCars.js
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Card, Table, Modal, Button, Form } from "react-bootstrap";
+import { Table, Modal, Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { FaEdit, FaTrash } from "react-icons/fa"; // Import delete icon
+import { FaEdit, FaTrash } from "react-icons/fa";
 import HostNavbar from "./navbar";
 
 const MyCars = ({ customerId }) => {
     const [myCars, setMyCars] = useState([]);
-    const [showModal, setShowModal] = useState(false);
+    const [showEditModal, setShowEditModal] = useState(false);
+    const [showViewModal, setShowViewModal] = useState(false);
     const [showDeleteModal, setShowDeleteModal] = useState(false);
     const [selectedCar, setSelectedCar] = useState(null);
     const [editedCar, setEditedCar] = useState({
@@ -19,6 +20,7 @@ const MyCars = ({ customerId }) => {
         source: "",
         price: "",
     });
+    const [bookingDetails, setBookingDetails] = useState(null);
 
     useEffect(() => {
         const uid = localStorage.getItem("id");
@@ -28,7 +30,6 @@ const MyCars = ({ customerId }) => {
             try {
                 const response = await axios.get(`http://localhost:9191/car/getall/` + hid);
                 setMyCars(response.data);
-                console.log("response.data", response.data);
             } catch (error) {
                 console.error('Error fetching booking history:', error);
             }
@@ -48,11 +49,13 @@ const MyCars = ({ customerId }) => {
             source: carToEdit.source,
             price: carToEdit.price,
         });
-        setShowModal(true);
+        setShowEditModal(true);
     };
 
     const handleModalClose = () => {
-        setShowModal(false);
+        setShowEditModal(false);
+        setShowViewModal(false);
+        setShowDeleteModal(false);
         setSelectedCar(null);
         setEditedCar({
             carModel: "",
@@ -60,7 +63,7 @@ const MyCars = ({ customerId }) => {
             seating: "",
             vehicleNo: "",
             source: "",
-            price:"",
+            price: "",
         });
     };
 
@@ -69,22 +72,17 @@ const MyCars = ({ customerId }) => {
             const uid = localStorage.getItem("id");
             const hid = parseInt(uid, 10) + 1;
 
-            // Update the editedCar object with source and price
             const updatedCar = {
                 ...editedCar,
                 source: editedCar.source,
                 price: editedCar.price,
             };
 
-            // Make the API call to update the car
             await axios.put(`http://localhost:9191/car/update/${hid}/${selectedCar.carId}`, updatedCar);
 
-            // Optionally, you can perform additional actions after a successful update
-
             console.log(`Car with ID ${selectedCar.carId} updated successfully`);
-            
-            handleModalClose(); // Close the modal after a successful update
-            
+
+            handleModalClose();
             window.location.reload();
         } catch (error) {
             console.error(`Error updating car with ID ${selectedCar.carId}:`, error);
@@ -99,26 +97,39 @@ const MyCars = ({ customerId }) => {
 
     const handleDeleteConfirm = async () => {
         try {
-            // Make the API call to delete the car
             const uid = localStorage.getItem("id");
             const hid = parseInt(uid, 10) + 1;
 
             await axios.delete(`http://localhost:9191/host/delete/${hid}/${selectedCar.carId}`);
 
-            // Optionally, you can perform additional actions after a successful delete
-
             console.log(`Car with ID ${selectedCar.carId} deleted successfully`);
-            
-            setShowDeleteModal(false); // Close the delete confirmation modal
-            window.location.reload(); // Reload the page to fetch updated data
+
+            setShowDeleteModal(false);
+            window.location.reload();
         } catch (error) {
             console.error(`Error deleting car with ID ${selectedCar.carId}:`, error);
         }
     };
 
-    const handleDeleteCancel = () => {
-        setShowDeleteModal(false);
-        setSelectedCar(null);
+    const handleViewBookings = async (carId) => {
+        try {
+            const uid = localStorage.getItem("id");
+            const hid = parseInt(uid, 10) + 1;
+
+            const response = await axios.get(`http://localhost:9191/host/customers/${hid}/${carId}`);
+            if (response.data[0]?.customer) {
+                setBookingDetails(response.data);
+                console.log("customer bookings:", response.data);
+                setShowViewModal(true);
+            } else {
+                // Display an alert when customer details are not available
+                alert("No Bookings yet");
+            }
+        } catch (error) {
+            console.error(`Error fetching bookings for car with ID ${carId}:`, error);
+            // Display an alert when bookings are not available
+            alert("Bookings are not available");
+        }
     };
 
     return (
@@ -139,6 +150,7 @@ const MyCars = ({ customerId }) => {
                         <th>Seating</th>
                         <th>Edit</th>
                         <th>Delete</th>
+                        <th>View Bookings</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -160,13 +172,18 @@ const MyCars = ({ customerId }) => {
                                     <FaTrash />
                                 </Link>
                             </td>
+                            <td>
+                                <Button variant="info" onClick={() => handleViewBookings(booking.carId)}>
+                                    View Bookings
+                                </Button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
             </Table>
 
             {/* Edit Car Modal */}
-            <Modal show={showModal} onHide={handleModalClose}>
+            <Modal show={showEditModal} onHide={handleModalClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>Edit Car Details</Modal.Title>
                 </Modal.Header>
@@ -175,7 +192,9 @@ const MyCars = ({ customerId }) => {
                         <Form.Group controlId="formCarModel">
                             <Form.Label>Car Model</Form.Label>
                             <Form.Control
-                                type="text" readOnly disabled
+                                type="text"
+                                readOnly
+                                disabled
                                 placeholder="Enter Car Model"
                                 value={editedCar.carModel}
                                 onChange={(e) => setEditedCar({ ...editedCar, carModel: e.target.value })}
@@ -226,7 +245,7 @@ const MyCars = ({ customerId }) => {
                                 onChange={(e) => setEditedCar({ ...editedCar, price: e.target.value })}
                             />
                         </Form.Group>
-                    </Form>
+                        </Form>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleModalClose}>
@@ -238,8 +257,42 @@ const MyCars = ({ customerId }) => {
                 </Modal.Footer>
             </Modal>
 
+            {/* View Bookings Modal */}
+            <Modal show={showViewModal} onHide={handleModalClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Booking Details</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {bookingDetails && (
+                        <div>
+                            <h5>Customer Details:</h5>
+                            <p>Name: {bookingDetails[0].customer.name}</p>
+                            <p>Email: {bookingDetails[0].customer.email}</p>
+                            <p>Age: {bookingDetails[0].customer.age}</p>
+                            <p>Phone No: {bookingDetails[0].customer.phoneNo}</p>
+
+                            <h5>Booking Details:</h5>
+                            <p>Car Model: {bookingDetails[0].car.carModel}</p>
+                            <p>Price: {bookingDetails[0].price}</p>
+                            <p>Fuel Type: {bookingDetails[0].car.fuelType}</p>
+                            <p>Seating: {bookingDetails[0].car.seating}</p>
+                            <p>Vehicle No: {bookingDetails[0].car.vehicleNo}</p>
+                            <p>Source: {bookingDetails[0].source}</p>
+                            <p>Destination: {bookingDetails[0].destination}</p>
+                            <p>From Date: {bookingDetails[0].fromDate}</p>
+                            <p>To Date: {bookingDetails[0].toDate}</p>
+                        </div>
+                    )}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleModalClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
             {/* Delete Confirmation Modal */}
-            <Modal show={showDeleteModal} onHide={handleDeleteCancel}>
+            <Modal show={showDeleteModal} onHide={handleModalClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>Delete Car</Modal.Title>
                 </Modal.Header>
@@ -247,7 +300,7 @@ const MyCars = ({ customerId }) => {
                     <p>Are you sure you want to delete the car?</p>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleDeleteCancel}>
+                    <Button variant="secondary" onClick={handleModalClose}>
                         Cancel
                     </Button>
                     <Button variant="danger" onClick={handleDeleteConfirm}>
